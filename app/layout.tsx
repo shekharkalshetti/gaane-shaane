61ff3cc26e09fca90580b6103fe1467366033e4b
Shekhar <shekkharkalshetti@gmail.com>
2023-11-02 18:32:03 +0530

    added supabase and hooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import { ModalProvider } from "@/providers/ModalProvider";
 
-const font = Figtree({ subsets: ["latin"] });
+const font = Figtree({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Spotify Clone",
@@ -18,7 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
